ed00f36d906b68308f55e24c06fab68dec53aa59
Wanderson David <wandersondavid.ifg@gmail.com>
2021-06-30 14:34:45 -0300

    style: ajustes spacamento

--- src/components/Category/styles.ts
+++ src/components/Category/styles.ts
@@ -4,14 +4,13 @@ import { theme } from '../../global/styles/theme';
 
 export const styles = StyleSheet.create({
   container: {
-
     width: 104,
     height: 120,
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 8,
-    marginRight: 8
-
+    marginRight: 8,
+    marginLeft:3,
   },
   content: {
     width: 100,
