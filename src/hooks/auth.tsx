2e7ebfa72a896d5fa38cb84c974d19d137dc75b3
Wanderson David <wandersondavid.ifg@gmail.com>
2021-07-02 14:22:13 -0300

    feat: add variavel de ambient

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -7,13 +7,13 @@ import React,
 } from 'react';
 
 import * as AuthSession from 'expo-auth-session'
-import {
-  REDIRECT_URI,
-  SCOPE,
-  RESPONSE_TYPE,
-  CLIENT_ID,
-  CDN_IMAGE,
-} from '../config'
+
+const { REDIRECT_URI } = process.env;
+const { SCOPE } = process.env;
+const { RESPONSE_TYPE } = process.env;
+const { CLIENT_ID } = process.env;
+const { CDN_IMAGE } = process.env;
+
 import { api } from '../services/api';
 
 type User = {
