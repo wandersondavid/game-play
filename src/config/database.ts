626d692a30f1cedc95a5166ef14cb61db9d8fdf1
Wanderson David <wandersondavid.ifg@gmail.com>
2021-07-02 14:47:48 -0300

    feat: salvando dados do user no dispositivo

---
+++ src/config/database.ts
@@ -0,0 +1,11 @@
+import { api } from "../services/api";
+
+const DATABASE_NAME = '@gameplay';
+
+const COLLECTION_USERS = `${DATABASE_NAME}:user`
+const COLLECTION_APPOINTMENTS = `${DATABASE_NAME}:appointments`
+
+export {
+  COLLECTION_USERS,
+  COLLECTION_APPOINTMENTS
+}
--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -3,10 +3,13 @@ import React,
   createContext,
   useContext,
   useState,
-  ReactNode
+  ReactNode,
+  useEffect
 } from 'react';
 
 import * as AuthSession from 'expo-auth-session'
+import AsyncStore from '@react-native-async-storage/async-storage'
+import { COLLECTION_USERS } from '../config/database'
 
 const { REDIRECT_URI } = process.env;
 const { SCOPE } = process.env;
@@ -62,12 +65,16 @@ const AuthProvider = ({ children }: AuthProviderProps) => {
 
         const firstName = userInfo.data.username.split(' ')[0];
         userInfo.data.avatar = `${CDN_IMAGE}/avatars/${userInfo.data.id}/${userInfo.data.avatar}.png`
-        setUser({
+
+        const userData = {
           ...userInfo.data,
           firstName,
           token: params.access_token
-        })
+        }
 
+        await AsyncStore.setItem(COLLECTION_USERS, JSON.stringify(userData))
+
+        setUser(userData)
       }
 
     } catch {
@@ -76,6 +83,20 @@ const AuthProvider = ({ children }: AuthProviderProps) => {
       setLoading(false);
     }
   }
+
+  const loadUserStorageData = async () => {
+    const storage = await AsyncStore.getItem(COLLECTION_USERS);
+
+    if(storage) {
+      const userLogged = JSON.parse(storage) as User
+      api.defaults.headers.authorization = `Bearer ${userLogged.token}`
+      setUser(userLogged);
+    }
+  }
+
+  useEffect(() => {
+    loadUserStorageData();
+  }, [])
 
   return (
     <AuthContext.Provider value={{
